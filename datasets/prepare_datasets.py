4d9aa2cd1513a9f1bf4e720215d2b041d6293826
nimishsantosh107 <nimishsantosh107@icloud.com>
2021-05-22 03:58:27 +0530

    new sizes specified, PRE TEST 0

--- datasets/prepare_datasets.py
+++ datasets/prepare_datasets.py
@@ -2,5 +2,6 @@ from preparation.download_image import download_dataset
 from preparation.make_patches import make_patches
 
 if __name__ == '__main__':
-    download_dataset()
-    make_patches()
+    # download_dataset()
+    # make_patches()
+    pass
